df7e3527dcd637751a8b8ad819a928aaa271124e
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-06-25 11:53:29 +0200

    asyn, await, fetch()

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document.querySelector("#generate").addEventListener("click", () => {
 
 document.querySelector("#recharge").addEventListener("click", () => {
     document.location.reload();
-})*/
+})
 
 function faireOperation1(init, callback) {
     const resultat = init + 1;
@@ -81,5 +81,61 @@ function faireOperation() {
 operationStart();
 
 
-/* Les callbacks sont penibles on appelle ça callback hell, c'est pourquoi de nos jours les 
-frameworks utilisent les promesses */
+Les callbacks sont penibles on appelle ça callback hell, c'est pourquoi de nos jours les 
+frameworks utilisent les promesses
+
+PARTIE FETCH 
+
+- On affecte la valeur du retour à fetchPromise
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+console.log(fetchPromise);
+-fonction de gestion ou gestionnaire
+fetchPromise.then(reponse => {
+    console.log(`Voici ce qu'on obtient en faisant la requête HTTP : ${reponse.status}`);
+});
+
+console.log('Requête initiée..');
+
+
+METHODE CATCH
+
+const fetchPromise = fetch('https://google.com');
+
+fetchPromise
+    .then(json => {
+        console.log(json[0].name);
+    })
+
+    .catch(error => {
+        console.error(`Malheureseument une erreur a été détecté => ${error}`)
+    });
+
+const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
+
+Promise.all([fetchPromise1, fetchPromise2])
+    .then(reponses => {
+        for (const reponse of reponses) {
+            console.log(`${reponse.url} : ${reponse.status}`);
+        }
+    })
+    .catch(error => {
+        console.error(`Une erreur a été détecté => ${error}`)    
+})*/
+
+async function maFonction() {
+    try{
+        const reponse = await fetch(`https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json`)
+        if(!(await reponse).ok) {
+            throw new Error(`Erreur HTTP : ${reponse.status}`);
+        }
+
+        const jsonReponse = await reponse.json();
+        console.log(jsonReponse[1].name);
+    }
+    catch(error) {
+        console.error(`Voici l'erreur détecté => ${error}`)
+    }
+}
+
+maFonction();
